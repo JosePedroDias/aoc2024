608ed37b871548caa783f2b63f557bdfb6f6acdd
José Pedro Dias <jose.pedro.dias@gmail.com>
2024-12-06 08:11:51 +0000

    day 6 part 1

--- src/Day06.kt
+++ src/Day06.kt
@@ -1,7 +1,120 @@
-class Day06 {
-    fun part1(input: List<String>): Int {
-        return 0
+private data class Pos (var x: Int, var y: Int)
+
+private enum class Dir(val x:Int, val y:Int) {
+    N( 0, -1),
+    S( 0,  1),
+    W(-1,  0),
+    E( 1,  0);
+
+    fun turnRight(): Dir {
+        return when (this) {
+            N -> E
+            E -> S
+            S -> W
+            W -> N
+        }
     }
-    //check(143 == part1(readInput("06_test")))
-    //println("part 1 answer: ${part1(readInput("06"))}")
+
+    fun moveForward(p: Pos): Pos {
+        return Pos(p.x + x, p.y + y)
+    }
+}
+
+private const val EMPTY = '.'
+private const val OBSTACLE = '#'
+
+private fun guardCharFromDir(d: Dir): Char {
+    return when(d) {
+        Dir.N -> '^'
+        Dir.E -> '>'
+        Dir.S -> 'v'
+        Dir.W -> '<'
+    }
+}
+
+private class Matrix(lines: List<String>) {
+    val w: Int = lines[0].length
+    val h: Int = lines.size
+    val m = Array(h) { CharArray(w) }
+    var dir: Dir = Dir.N
+    var pos: Pos = Pos(0, 0)
+
+    init {
+        lines.forEachIndexed() {
+                y, line ->
+            val row = m[y]
+            line.forEachIndexed { x, ch ->
+                if (ch == '^') {
+                    dir = Dir.N
+                    pos = Pos(x, y)
+                } else if (ch == '>') {
+                    dir = Dir.E
+                    pos = Pos(x, y)
+                } else if (ch == 'v') {
+                    dir = Dir.S
+                    pos = Pos(x, y)
+                } else if (ch == '<') {
+                    dir = Dir.W
+                    pos = Pos(x, y)
+                }
+                row[x] = ch
+            }
+        }
+    }
+
+    fun inBounds(p: Pos): Boolean {
+        return p.x in 0..< w && p.y in 0..< h
+    }
+
+    fun g(p: Pos): Char {
+        return m[p.y][p.x]
+    }
+
+    fun s(p:Pos, c:Char) {
+        if (!inBounds(p)) {
+            throw Error("pos is out of bounds")
+        }
+        m[p.y][p.x] = c
+    }
+
+    override fun toString(): String {
+        return StringBuilder().apply {
+            for (y in 0..< h) {
+                for (x in 0..< w) {
+                    append(g(Pos(x, y)))
+                }
+                append('\n')
+            }
+        }.toString()
+    }
+}
+
+fun main() {
+    fun part1(input: List<String>): Int {
+        val m = Matrix(input)
+        // println("pos:${m.pos} | dir:${m.dir} ${m.dir.x},${m.dir.y}"); println(m)
+
+        val positions = mutableSetOf(m.pos)
+        while (true) {
+            val pos2 = m.dir.moveForward(m.pos)
+            try {
+                val charAtPos2 = m.g(pos2)
+                if (charAtPos2 == OBSTACLE) {
+                    m.dir = m.dir.turnRight()
+                    // println("OBSTACLE: TURN RIGHT TO ${m.dir}"); println(m)
+                } else {
+                    m.s(m.pos, EMPTY)
+                    m.pos = pos2
+                    m.s(m.pos, guardCharFromDir(m.dir))
+                    positions.add(m.pos)
+                    // println("EMPTY: MOVE FORWARD ${m.dir} TO ${m.pos}"); println(m)
+                }
+            } catch (e: Throwable) {
+                // println("OUT OF BOUNDS: ALL DONE"); println(positions)
+                return positions.size
+            }
+        }
+    }
+    check(41 == part1(readInput("06_test")))
+    println("part 1 answer: ${part1(readInput("06"))}")
 }

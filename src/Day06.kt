1033652a3d0106535bf243c355830e97c3b73506
José Pedro Dias <jose.pedro.dias@gmail.com>
2024-12-06 07:25:09 +0000

    day 6

---
+++ src/Day06.kt
@@ -0,0 +1,7 @@
+class Day06 {
+    fun part1(input: List<String>): Int {
+        return 0
+    }
+    //check(143 == part1(readInput("06_test")))
+    //println("part 1 answer: ${part1(readInput("06"))}")
+}

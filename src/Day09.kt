101343daaa0ac74d05162c45d31fbb772a715b71
José Pedro Dias <jose.pedro.dias@gmail.com>
2024-12-09 22:18:43 +0000

    part 2 done. ufff. i must have over complicated things

--- src/Day09.kt
+++ src/Day09.kt
@@ -57,26 +57,79 @@ private fun defrag(disk0: Disk): Disk {
     return disk
 }
 
+/*
+    A a        b B
+    NNN        444
+
+00...111...2...333.44.5555.6666.777.888899          B:41  99   40-41 ->  2- 4  b=40-1
+0099.111...2...333.44.5555.6666.777.8888..          B:39  8888 36-39 -> NO     b=36-1
+                                                    B:35  777  32-34 ->  9-11  b=32-1
+0099.1117772...333.44.5555.6666.....8888..          B:31  6666 27-30 -> NO     b=27-1
+                                                    B:26  5555 22-25 -> NO     b=22-1
+                                                    B:21  44   19-20 -> 12-13  b=19-1
+0099.111777244.333....5555.6666.....8888..          B:18  333  15-17 -> NO     b=15-1
+00992111777.44.333....5555.6666.....8888..          B:14  2    11-11 ->  4- 4  b=11-1
+                                                    B:10  111   5- 7 -> NO     b= 5-1
+                                                    B: 4  00    0- 1 ->
+*/
 private fun defrag2(disk0: Disk): Disk {
     val disk = disk0.clone()
-    var b = disk.size - 1 // from index (value is not null)
-    var a = 0 // to index (value is null)
-    while (b >= a) {
-        val vb = disk[b]
-        if (vb != null) {
-            disk[b] = null
-            var va = disk[a]
-            while (va != null) {
-                ++a
-                va = disk[a]
+
+    var candidateEnd = disk.size - 1 // from index (value is not null)
+
+    // find last vb other than null
+    var ivb = candidateEnd
+    while (disk[ivb] == null) { --ivb }
+    var vb = disk[ivb]!!
+
+    while (candidateEnd >= 0) {
+        //println(diskToString(disk))
+
+        // find candidate
+        while (disk[candidateEnd] != vb) { --candidateEnd }
+        var candidateStart = candidateEnd
+        while (candidateStart > 0 && disk[candidateStart-1] == vb) { --candidateStart }
+        val candidateSize = candidateEnd - candidateStart + 1
+        //println("** candidate of $vb's [$candidateStart,$candidateEnd] (len: $candidateSize) **")
+
+        // find gap
+        var gapStart = 0
+        var gapSize = 0
+        do {
+            while (disk[gapStart] != null) { ++gapStart }
+            var gapEnd = gapStart
+            while (gapEnd + 1 < disk.size && disk[gapEnd + 1] == null) { ++gapEnd }
+            if (gapEnd - gapStart + 1 < candidateSize) {
+                //println("( gap [$gapStart, $gapEnd] is too small )")
+                if (gapEnd + 1 > candidateStart) { break }
+                gapStart = gapEnd + 1
+            } else if (gapEnd > candidateStart) {
+                //println("( gap only to the right )")
+                break
+            } else {
+                gapSize = gapEnd - gapStart + 1
+                //println("-> found gap of null's [$gapStart,$gapEnd] (len: ${gapEnd - gapStart + 1})")
             }
-            if (a <= b) {
-                disk[a] = vb
-                ++a
-                //if (disk.size < 100) { println(diskToString(disk)) }
+        } while (gapSize == 0)
+
+        if (gapSize > 0) {
+            // fits: fill it and move nonNullStart forward
+            //println("-> fits")
+            for (i in 0 until candidateSize) {
+                disk[candidateEnd - i] = null
+                disk[gapStart + i] = vb
             }
+        } else {
+            // does not fit: ignore candidate
+            //println("-> no gap!")
         }
-        --b
+
+        --vb
+        if (vb < 0) {
+            //println("all done")
+            return disk
+        }
+        candidateEnd = candidateStart - 1
     }
     return disk
 }
@@ -98,7 +151,7 @@ fun main() {
         val sT1 = readInputAsString("09_test")
         val dT1 = toDisk(sT1)
         val dT1S = diskToString(dT1)
-        println(dT1S)
+        //println(dT1S)
         check(dT1S == "00...111...2...333.44.5555.6666.777.888899")
         val dT2 = defrag(dT1)
         val dT2S = diskToString(dT2)

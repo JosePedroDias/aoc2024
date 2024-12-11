12a6f63a60abde2e7dd989d79591a98ac95348d7
José Pedro Dias <jose.pedro.dias@gmail.com>
2024-12-11 08:12:38 +0000

    move to ints; call gc between blinks. still breaks

--- src/Day11.kt
+++ src/Day11.kt
@@ -13,59 +13,65 @@ nr of stones after 25 iterations?
 1 2024 1 0 9 9 2021976
  */
 
-private fun parse(line: String): List<Long> {
-    return line.split(" ").map { it.toLong() }
+private fun parse(line: String): List<Int> {
+    return line.split(" ").map { it.toInt() }
 }
 
-private fun nrDigits(n: Long): Int {
+private fun nrDigits(n: Int): Int {
     return n.toString().length
 }
 
-private fun nrDigits2(n: Long): Int {
-    return if (n == 0L) 0 else log10(n.toDouble()).toInt() + 1
+private fun nrDigits2(n: Int): Int {
+    return if (n == 0) 0 else log10(n.toDouble()).toInt() + 1
 }
 
-private fun splitEvenlySizedNumber(n: Long): List<Long> {
+private fun splitEvenlySizedNumber(n: Int): List<Int> {
     val s = n.toString()
     val i = s.length / 2
     val a = s.substring(0, i)
     val b = s.substring(i)
-    return listOf(a.toLong(), b.toLong())
+    return listOf(a.toInt(), b.toInt())
 }
 
-private fun splitEvenlySizedNumber2(n: Long): List<Long> {
+private fun splitEvenlySizedNumber2(n: Int): List<Int> {
     val nrDi = nrDigits(n)
-    val order = 10.0.pow(nrDi/2).toLong()
+    val order = 10.0.pow(nrDi/2).toInt()
     val a = n / order
     val b = n - a * order
     return listOf(a, b)
 }
 
-private fun step(stones: List<Long>): List<Long> {
+private fun step(stones: List<Int>): List<Int> {
     return stones.flatMap {
         when {
-            it == 0L -> listOf(1L)
+            it == 0 -> listOf(1)
             nrDigits2(it) % 2 == 0 -> splitEvenlySizedNumber2(it)
-            else -> listOf(2024L * it)
+            else -> listOf(2024 * it)
         }
     }
 }
 
 private fun blinkNTimes(line: String, times: Int): Int {
     var stones = parse(line)
-    repeat(times) { stones = step(stones) }
+    var i = 0
+    repeat(times) {
+        ++i
+        println("time: $i")
+        System.gc()
+        stones = step(stones)
+    }
     return stones.size
 }
 
 fun main() {
     val dt = measureTime {
-        check(nrDigits(2314L) == 4)
-        check(nrDigits2(2314L) == 4)
+        check(nrDigits(2314) == 4)
+        check(nrDigits2(2314) == 4)
 
-        check(splitEvenlySizedNumber(2314L)[0] == 23L)
-        check(splitEvenlySizedNumber(2314L)[1] == 14L)
-        check(splitEvenlySizedNumber2(2314L)[0] == 23L)
-        check(splitEvenlySizedNumber2(2314L)[1] == 14L)
+        check(splitEvenlySizedNumber(2314)[0] == 23)
+        check(splitEvenlySizedNumber(2314)[1] == 14)
+        check(splitEvenlySizedNumber2(2314)[0] == 23)
+        check(splitEvenlySizedNumber2(2314)[1] == 14)
 
         check(55312 == blinkNTimes(readInputAsString("11_test"), 25))
 

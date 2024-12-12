521adf7b81e21c9779a38300d1e631bb98583262
José Pedro Dias <jose.pedro.dias@gmail.com>
2024-12-12 21:16:23 +0000

    fixed islands

--- src/Day12.kt
+++ src/Day12.kt
@@ -4,11 +4,13 @@ private data class Pos3(val x: Int, val y: Int)
 //private data class Rect(val x: Int, val y: Int, val w: Int, val h: Int)
 private typealias Island = Set<Pos3>
 
+private data class MutablePair(var first: Int, var second: Int)
+
 private data class Matrix4(val w: Int, val h: Int) {
     val m: MutableMap<Pos3, Char> = mutableMapOf()
     val m2: MutableMap<Char, MutableList<Pos3>> = mutableMapOf()
-    val xRange = 0..< w
-    val yRange = 0..< h
+    var xRange = 0..< w
+    var yRange = 0..< h
 
     fun s(p: Pos3, ch: Char) {
         m[p] = ch
@@ -21,7 +23,7 @@ private data class Matrix4(val w: Int, val h: Int) {
     }
 
     fun g(p: Pos3): Char {
-        return m.getOrDefault(p, ' ')
+        return m.getOrDefault(p, '.')
     }
 
     fun inBounds(p: Pos3): Boolean {
@@ -51,6 +53,21 @@ private data class Matrix4(val w: Int, val h: Int) {
         .forEach { yield(it) }
     }
 
+    fun getBoundary(cells: Collection<Pos3>): Pair<IntRange, IntRange> {
+        val xLims = MutablePair(Int.MAX_VALUE, Int.MIN_VALUE)
+        val yLims = MutablePair(Int.MAX_VALUE, Int.MIN_VALUE)
+        for ((x, y) in cells) {
+            if (x < xLims.first) xLims.first = x
+            if (x > xLims.second) xLims.second = x
+            if (y < yLims.first) yLims.first = y
+            if (y > yLims.second) yLims.second = y
+        }
+        return Pair(
+            xLims.first..xLims.second,
+            yLims.first .. yLims.second,
+        )
+    }
+
     fun findIslands(ch: Char) = sequence {
         val visited = mutableSetOf<Pos3>()
         val toVisit = mutableSetOf<Pos3>()
@@ -58,15 +75,19 @@ private data class Matrix4(val w: Int, val h: Int) {
         var island = mutableSetOf<Pos3>()
         while (positionsWithThisValue.size > 0) {
             val p0 = positionsWithThisValue[0]
-            toVisit.add(p0)
             positionsWithThisValue.remove(p0)
-            for (p in toVisit.toList()) {
-                neighborsWithValue(p, ch).forEach { if (!visited.contains(it)) toVisit.add(it) }
-                island.add(p)
-                visited.add(p)
-                toVisit.remove(p)
-                positionsWithThisValue.remove(p)
-            }
+            toVisit.add(p0)
+            do {
+                for (p in toVisit.toList()) {
+                    toVisit.remove(p)
+                    neighborsWithValue(p, ch).forEach {
+                        if (!visited.contains(it)) toVisit.add(it)
+                    }
+                    island.add(p)
+                    visited.add(p)
+                    positionsWithThisValue.remove(p)
+                }
+            } while (toVisit.size > 0)
             yield(island)
             island = mutableSetOf()
         }
@@ -99,12 +120,32 @@ private fun parse(lines: List<String>): Matrix4 {
 fun main() {
     val dt = measureTime {
         val lines = readInput("12_test")
+        //val lines = readInput("12_test2")
+        //val lines = readInput("12_test3")
+        //val lines = readInput("12")
         val m = parse(lines)
         println(m)
         val chars = m.chars().toMutableList()
         for (ch in chars) {
-            println("\nislands with $ch...")
-            println( m.findIslands(ch).toList() )
+            //println("\nislands with $ch...")
+            for (island in m.findIslands(ch)) {
+                //println(island)
+                val bounds = m.getBoundary(island)
+                val m2 = Matrix4(
+                    bounds.first.last - bounds.first.first + 1,
+                    bounds.second.last - bounds.second.first + 1,
+                )
+                for (p in island) {
+                    val p0 = Pos3(
+                        p.x - bounds.first.first,
+                        p.y - bounds.second.first,
+                    )
+                    m2.s(p0, ch)
+                }
+                println(m2)
+                println()
+            }
+            //println( m.findIslands(ch).toList() )
         }
     }
     println(dt)

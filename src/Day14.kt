b51f955ab2f935e545c7b3027502b3e069fc1918
José Pedro Dias <jose.pedro.dias@gmail.com>
2024-12-14 11:21:53 +0000

    end of part 1

--- src/Day14.kt
+++ src/Day14.kt
@@ -1,5 +1,7 @@
 import kotlin.time.measureTime
 
+private const val STEPS = 100
+
 // https://kotlinlang.org/docs/operator-overloading.html
 private data class Pos6(var x:Int = 0, var y:Int = 0) {
     override fun toString(): String {
@@ -29,7 +31,14 @@ private data class Robot(val p: Pos6, val v:Pos6)
 
 private data class Matrix5(val w: Int, val h: Int) {
     var ranges = arrayOf(0..< w, 0..< h)
+    var middles = arrayOf(ranges[0].last / 2, ranges[1].last / 2)
+    var drawQuadrants: Boolean = false
     val robots = mutableListOf<Robot>()
+
+    init {
+        check(w % 2 == 1)
+        check(h % 2 == 1)
+    }
 
     fun addRobot(r: Robot) {
         robots.add(r)
@@ -42,16 +51,55 @@ private data class Matrix5(val w: Int, val h: Int) {
         }
     }
 
+    fun quadrants() = sequence {
+        val (middleX, middleY) = middles
+
+        val rx0 = 0 ..< middleX
+        val rx1 = middleX+1 .. ranges[0].last
+
+        val ry0 = 0 ..< middleY
+        val ry1 = middleY+1 .. ranges[1].last
+
+        yield(Pair(rx0, ry0))
+        yield(Pair(rx1, ry0))
+        yield(Pair(rx0, ry1))
+        yield(Pair(rx1, ry1))
+    }
+
+    fun answer(): Long {
+        //drawQuadrants = true; println(this); drawQuadrants = false
+
+        val qs = quadrants().toList()
+        val qCounts = qs.map { (rx, ry) ->
+            var sum = 0L
+            for (y in ry.first..ry.last) {
+                for (x in rx.first..rx.last) {
+                    sum += countRobotsAt(Pos6(x, y))
+                }
+            }
+            sum
+        }
+        println("qCounts: $qCounts")
+
+        val answer = qCounts.reduce { acc, v -> acc * v }
+        println("answer: $answer")
+        return answer
+    }
+
+    fun countRobotsAt(p: Pos6): Int {
+        return robots.count { it.p == p }
+    }
+
     override fun toString(): String {
         val sb = StringBuilder()
         for (y in 0..ranges[1].last) {
             for (x in 0..ranges[0].last) {
-                val p = Pos6(x, y)
-                val amount = robots.count { it.p == p }
-                if (amount > 0) {
-                    sb.append(amount)
+                if (drawQuadrants && (x == middles[0] || y == middles[1])) {
+                    sb.append(' ')
                 } else {
-                    sb.append('.')
+                    val amount = countRobotsAt(Pos6(x, y))
+                    if (amount > 0) sb.append(amount)
+                    else sb.append('.')
                 }
             }
             sb.append('\n')
@@ -75,10 +123,10 @@ private fun parse(lines: List<String>, dims: Pair<Int, Int>): Matrix5 {
 
 private fun simulate(m: Matrix5) {
     var t = 0
-    println("t=${t++}\n$m\n")
+    //println("t=${t++}\n$m\n")
     repeat(100) {
         m.moveTick()
-        println("t=${t++}\n$m\n")
+        //println("t=${t++}\n$m\n")
     }
 }
 
@@ -96,12 +144,11 @@ fun main() {
 
         val mt = parse(readInput("14t1"), Pair(11, 7))
         simulate(mt)
+        println("Answer to part 0: ${mt.answer()}")
 
-        //val m = parse(readInput("14"), Pair(101, 103))
-        //simulate(m)
-
-        val res1 = 0
-        println("Answer to part 1: $res1")
+        val m = parse(readInput("14"), Pair(101, 103))
+        simulate(m)
+        println("Answer to part 1: ${m.answer()}")
     }
     println(dt)
 }

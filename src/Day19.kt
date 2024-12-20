5f4107fb0be562157ac030dfeef6d2c2a5c85ba4
José Pedro Dias <jose.pedro.dias@gmail.com>
2024-12-20 01:27:19 +0000

    p1

--- src/Day19.kt
+++ src/Day19.kt
@@ -8,18 +8,34 @@ private fun parse(lines: List<String>): Puzzle19 {
     return Pair(alphabet, candidates)
 }
 
-private fun part1(p: Puzzle19): Int {
-    println(p)
-    // TODO
-    return 0
+private fun possible(pattern: String, alphabet: List<String>): Boolean {
+    val usefulParts = mutableSetOf<String>()
+    for (size in 1..pattern.length) {
+        for (j in 0..pattern.length - size) {
+            usefulParts.add(pattern.substring(j, j + size))
+        }
+    }
+    val filteredAlphabet = alphabet.filter { usefulParts.contains(it) }
+    //println("$pattern from: $filteredAlphabet ?")
+    val rgx = Regex("""(?:${filteredAlphabet.joinToString("|")})+""")
+    val m = rgx.matchEntire(pattern)
+    return m != null
+}
+
+private fun part1(puzzle: Puzzle19): Int {
+    val (alphabet, candidates) = puzzle
+    return candidates.count { possible(it, alphabet) }
 }
 
 fun main() {
     val dt = measureTime {
         val t1 = parse(readInput("19t1"))
         val r1 = part1(t1)
-        println(r1)
-        //val p = parse(readInput("19"))
+        check(r1 == 6)
+
+        val t = parse(readInput("19"))
+        val r = part1(t)
+        println("Answer to part 1: $r")
     }
     println(dt)
 }

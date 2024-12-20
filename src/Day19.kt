3dccafa10a7c43ff2b16f593ab0a0565a029da18
José Pedro Dias <jose.pedro.dias@gmail.com>
2024-12-20 01:33:39 +0000

    part 2 wip

--- src/Day19.kt
+++ src/Day19.kt
@@ -22,9 +22,57 @@ private fun possible(pattern: String, alphabet: List<String>): Boolean {
     return m != null
 }
 
+private fun possibilities1(goal: String, alphabet: List<String>, soFar: String = ""): Int {
+    if (soFar == goal) return 1
+    if (soFar.length > goal.length) return 0
+    var sum = 0
+    for (al in alphabet) {
+        val nxt = "$soFar$al"
+        if (
+            //soFar.length + al.length <= goal.length &&
+            goal.startsWith(nxt)) {
+            sum += possibilities1(goal, alphabet, nxt)
+        }
+    }
+    return sum
+}
+
+private fun possibilities2(goal: String, alphabet: List<String>): Int {
+    var sum = 0
+    val queue = ArrayDeque<String>()
+    queue.add("")
+
+    while (queue.isNotEmpty()) {
+        val current = queue.removeFirst()
+
+        when {
+            current == goal -> sum += 1
+            goal.startsWith(current) -> {
+                for (al in alphabet) {
+                    val next = current + al
+                    if (goal.startsWith(next)) {
+                        queue.add(next)
+                    }
+                }
+            }
+        }
+    }
+
+    return sum
+}
+
+
 private fun part1(puzzle: Puzzle19): Int {
     val (alphabet, candidates) = puzzle
     return candidates.count { possible(it, alphabet) }
+}
+
+private fun part2(puzzle: Puzzle19): Int {
+    val (alphabet, candidates) = puzzle
+    return candidates.mapIndexed { idx, c ->
+        println("${idx + 1}/${candidates.size}: $c")
+        possibilities2(c, alphabet)
+    }.sum()
 }
 
 fun main() {
@@ -36,6 +84,12 @@ fun main() {
         val t = parse(readInput("19"))
         val r = part1(t)
         println("Answer to part 1: $r")
+
+        val r1b = part2(t1)
+        check(r1b == 16)
+
+        val rb = part2(t)
+        println("Answer to part 2: $rb")
     }
     println(dt)
 }

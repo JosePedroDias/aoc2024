72dd1ae2e8da3e1b938be8a70019352194038b75
José Pedro Dias <jose.pedro.dias@gmail.com>
2024-12-30 18:54:42 +0000

    too slow without recursion but with strings

--- src/Day19.kt
+++ src/Day19.kt
@@ -1,3 +1,4 @@
+import java.util.*
 import kotlin.time.measureTime
 
 private typealias Puzzle19 = Pair<List<String>, List<String>>
@@ -8,53 +9,56 @@ private fun parse(lines: List<String>): Puzzle19 {
     return Pair(alphabet, candidates)
 }
 
-private fun possible(pattern: String, alphabet: List<String>): Boolean {
+// abc -> [a, b, c, ab, bc, abc]
+private fun subParts(word: String): Set<String> {
     val usefulParts = mutableSetOf<String>()
-    for (size in 1..pattern.length) {
-        for (j in 0..pattern.length - size) {
-            usefulParts.add(pattern.substring(j, j + size))
+    for (size in 1..word.length) {
+        for (j in 0..word.length - size) {
+            usefulParts.add(word.substring(j, j + size))
         }
     }
+    return usefulParts
+}
+
+private fun possible(pattern: String, alphabet: List<String>): Boolean {
+    val usefulParts = subParts(pattern)
     val filteredAlphabet = alphabet.filter { usefulParts.contains(it) }
-    //println("$pattern from: $filteredAlphabet ?")
     val rgx = Regex("""(?:${filteredAlphabet.joinToString("|")})+""")
     val m = rgx.matchEntire(pattern)
     return m != null
 }
 
-private fun possibilities1(goal: String, alphabet: List<String>, soFar: Int = 0): Int {
+private fun possibilities0(goal: String, alphabet: List<String>, soFar: Int = 0): Int {
     if (soFar == goal.length) return 1
     var sum = 0
     for (al in alphabet) {
         val nxt = soFar + al.length
         if (nxt <= goal.length && goal.substring(soFar, nxt) == al) {
-            //println(goal.substring(soFar, nxt))
-            sum += possibilities1(goal, alphabet, nxt)
+            //println(goal.substring(0, nxt))
+            sum += possibilities0(goal, alphabet, nxt)
         }
     }
     return sum
 }
 
-private tailrec fun possibilities2(
-    goal: String,
-    alphabet: List<String>,
-    stack: List<Int> = listOf(0),
-    sum: Int = 0
-): Int {
-    if (stack.isEmpty()) return sum
-
-    val current = stack.first()
-    val rest = stack.drop(1)
-
-    return if (current == goal.length) {
-        possibilities2(goal, alphabet, rest, sum + 1)
-    } else {
-        val nextStates = alphabet.mapNotNull { al ->
-            val nxt = current + al.length
-            if (nxt <= goal.length && goal.substring(current, nxt) == al) nxt else null
+private fun possibilities1(goal: String, alphabet: List<String>): Int {
+    var hitCount = 0
+    val goalSize = goal.length
+    val soFars = ArrayDeque(listOf(0))
+    while (soFars.size > 0) {
+        val soFar = soFars.poll()!!
+        for (al in alphabet) {
+            //print("\nsoFar:$soFar, al:$al")
+            val nxt = soFar + al.length
+            if (nxt > goalSize) continue
+            val testStr = goal.substring(0, soFar) + al
+            if (nxt == goalSize) {
+                if (goal == testStr) { ++hitCount /*; print(" HIT!")*/ }
+            }
+            else if (goal.substring(soFar, nxt) == al) soFars.add(nxt)
         }
-        possibilities2(goal, alphabet, rest + nextStates, sum)
     }
+    return hitCount
 }
 
 private fun part1(puzzle: Puzzle19): Int {
@@ -66,25 +70,26 @@ private fun part2(puzzle: Puzzle19): Int {
     val (alphabet, candidates) = puzzle
     return candidates.mapIndexed { idx, c ->
         println("${idx + 1}/${candidates.size}: $c")
-        possibilities1(c, alphabet)
+        possibilities0(c, alphabet)
+        //possibilities1(c, alphabet)
     }.sum()
 }
 
 fun main() {
     val dt = measureTime {
-        val t1 = parse(readInput("19t1"))
-        val r1 = part1(t1)
-        check(r1 == 6)
+        val iT1 = parse(readInput("19t1"))
+        val oT1 = part1(iT1)
+        check(oT1 == 6)
 
-        val t = parse(readInput("19"))
-        val r = part1(t)
-        println("Answer to part 1: $r")
+        val iP = parse(readInput("19"))
+        val oP = part1(iP)
+        println("Answer to part 1: $oP")
 
-        val r1b = part2(t1)
-        check(r1b == 16)
+        val oT1b = part2(iT1)
+        check(oT1b == 16, { "$oT1b != 16" })
 
-        val rb = part2(t)
-        println("Answer to part 2: $rb")
+        val oP2 = part2(iP)
+        println("Answer to part 2: $oP2")
     }
     println(dt)
 }
